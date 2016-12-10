f317632826d7cad769258df22af07f478b44d53d
drewmstiles <drewmstiles@gmail.com>
2016-12-10 03:52:38 -0800

    Implement visualization index selections

--- home.js
+++ home.js
@@ -1,3 +1,4 @@
+
 d3.selectAll(".homeKnow").on("click", function() {
 	d3.selectAll(".homeKnow").each(function() {
 		d3.select(this)
@@ -22,11 +23,35 @@ d3.selectAll(".homePer").on("click", function() {
 		.style("text-decoration", "underline");
 });
 
-
 d3.select("#visButton").on("click", function() {
-	var h = window.innerHeight;
-	d3.select("#home")
-		.transition()
-		.duration(1000)
-		.style("top", -h + "px");
+	
+	var city = d3.select("#homeCity").attr("value");
+	console.log(city);
+	
+	var know = getSelectedValueForClass("homeKnow")
+	console.log(know);
+	
+	var perspective = getSelectedValueForClass("homePer")
+	console.log(perspective);
+
+// 	var h = window.innerHeight;
+// 	d3.select("#home")
+// 		.transition()
+// 		.duration(1000)
+// 		.style("top", -h + "px");
 });
+
+
+function getSelectedValueForClass(className) {
+		var value = "";
+		d3.selectAll("." + className).each(function() {
+		var s = d3.select(this);
+		if (s.attr("x-data-selected") == "yes") {
+			value = s.attr("x-data-value");
+		}
+		else {
+			// Keep looking.
+		}
+	});
+	return value;
+}
--- nav.js
+++ nav.js
@@ -1,19 +1,5 @@
-
-var row = 0,
-	col = 0;
-	
-var navMap = [
-	[0,0],
-	[0,0]
-];
-
 $("#wrapper").css("width",window.innerWidth + "px");
 $("#wrapper").css("height",window.innerHeight + "px");
-
-$("#heatmap").css("top",window.innerHeight + "px");
-$("#futureMap").css("left",window.innerWidth + "px");
-$("#futureAbstract").css("top",window.innerHeight + "px");
-$("#futureAbstract").css("left",window.innerWidth + "px");
 
 $(document).on("click", "#lastYear", function() {
 	var yr = $("#year");
@@ -32,54 +18,3 @@ $(document).on("click", "#nextYear", function() {
 $(document).on("change", "#sev", render)
 
 $(document).on("change", "#fac", render);
-
-$(document).on("click", ".nav", function() {
-	orient($(this).attr("id"), this);
-});
-
-function orient(dir, elem) {
-
-	if ($(elem).hasClass("disabled")) {
-		// Do nothing.
-	}
-	else {
-		var fn;
-		var anim = {};
-		
-		console.log("row: " + row + " col " + col);
-		if (dir == "down" && (row + 1) < navMap.length) {
-			row += 1;
-			anim.top = "-=" + window.innerHeight;
-		}
-		else if (dir == "left" && (col - 1) >= 0) {
-			col -= 1;
-			anim.left = "+=" + window.innerWidth;
-		}
-		else if (dir == "right" && (col + 1) < navMap[0].length) {
-			col += 1;
-			anim.left = "-=" + window.innerWidth;
-		}
-		else if (dir == "up" && (row - 1)  >= 0) {
-			row -= 1;
-			anim.top = "+=" + window.innerHeight;
-		}
-		else {
-			console.log("Where are you going?");
-		}
-	
-		$(".view").animate(anim, 1000);
-		
-		setTimeout(function() {
-			console.log("comp");
-			if (row == 0 && col == 0) {
-				
-			}
-			else if (row == 0 && col == 1) {
-				
-			}
-			else if (row == 1 && col == 0) {
-				showHeatmap();
-			}
-		}, 1010);
-	}
-}
