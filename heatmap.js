5314ce44fd93f77b6c785fff5446af15e6ec9677
drewmstiles <drewmstiles@gmail.com>
2016-12-16 02:47:27 -0800

    Switch to 24 hour heatmap labels

--- heatmap.js
+++ heatmap.js
@@ -7,7 +7,7 @@ var heatMapMargin = { top: 80, right: 50, bottom: 0, left: 50 },
 	buckets = 9,
 	colors = ["black","white"],
 	heatmapDays = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"],
-	times = ["1a", "2a", "3a", "4a", "5a", "6a", "7a", "8a", "9a", "10a", "11a", "12a", "1p", "2p", "3p", "4p", "5p", "6p", "7p", "8p", "9p", "10p", "11p", "12p"];
+	times = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24"];
 	
 
 	if (heatMapGridSize == Math.floor((window.innerHeight/2 - heatMapMargin.top - heatMapMargin.bottom) / 7)) {
